e44fb728d93e926ce097af31cc4ff455f96d93da
andre <andrebertonhadev@gmail.com>
2020-10-20 04:57:54 -0700

    listando frameworks

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var client = redis.createClient();
 
 client.on('connect', function() {
     console.log('connected!')
-})
+});
 
 client.set('total', '1800');
 
 client.hmset('languages', 'portuguese', 'english', 'spanish', 'british')
 client.hgetall('languages', function(err, object) {
-    console.log(object)
-})
+    console.log(object);
+});
+
+client.rpush(['frameworks', 'angular', 'react', 'vue'], function (err, reply) {
+console.log(reply); 
+});
+
+client.lrange('frameworks', 0, -1, function(err, reply){
+    console.log(reply);
+});
